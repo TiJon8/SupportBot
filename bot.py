f70bf2b1d169c38fd88947f281c76b24c4820aec
Syndicate <alex.gromov2@mail.ru>
2023-07-26 00:18:08 +0300

    added bot.py and start processing

--- bot.py
+++ bot.py
@@ -1,4 +1,14 @@
-a = 1
-b = 'lol'
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
-print(str(a) + b)
+bot = Bot(token='5343916790:AAF-Ua72J2AHwhYMw1RFqACfhC33Fk9aRVI')
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(content_types=['text'])
+async def send_hello_message(message: types.Message):
+    await bot.send_message(message.chat.id, message)
+    await bot.send_message(message.chat.id, 'Привет, мир!')
+
+executor.start_polling(dispatcher=dp)
